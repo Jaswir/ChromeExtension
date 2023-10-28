b4b9fd441ecb3d777d5b09520ffad37a80088959
Jaswir Raghoe <jaswirraghoe@gmail.com>
2023-10-28 20:37:55 +0200

    Change the first text of messages to thumbs up

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function makeBlankPage() {
-    var body = document.body;
-
-    // Remove all child nodes from the body
-    while (body.firstChild) {
-        body.removeChild(body.firstChild);
-    }
-}
-
-function changeBackgroundColor(color) {
-    document.body.style.backgroundColor = color
-    console.log("Jaswir is the best")
-}
 
 // chrome.extension.getBackgroundPage().console.log('foo');
 
@@ -25,9 +12,42 @@ function changeBackground(color) {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
-            function: changeBackgroundColor,
-            args: [color],
+            function: changeText,
+            // args: [color],
         })
     })
 }
 
+function makeBlankPage() {
+    var body = document.body;
+
+    // Remove all child nodes from the body
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+}
+
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color
+
+    console.log("Jaswir is the best")
+    console.log("Message Dom: ")
+}
+
+function changeText() {
+
+    // Element with this custom html property and value contains all messages
+    var messageContainer = document.querySelectorAll('[aria-live="polite"]')[0]
+
+    console.log("Message Container: ")
+    console.log(messageContainer)
+
+    //Change text of first message
+    if (messageContainer.children.length != 0) {
+        var text0 = messageContainer.children[0].
+            children[1].innerText = "thumbs up"
+    }
+
+
+
+}
