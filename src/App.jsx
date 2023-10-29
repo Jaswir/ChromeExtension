65635c49ec59d0ca9b8de207344e26b2aed15e71
Jaswir Raghoe <jaswirraghoe@gmail.com>
2023-10-29 18:20:35 +0100

    Build extension in react

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,54 @@ function App() {
     let [tabs] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
       target: { tabId: tabs.id },
-      args: [colour],
-      func: (colour) => {
-        document.body.style.backgroundColor = colour;
-      }
+      function: addLikeButton,
     })
   }
+
+  function addLikeButton() {
+
+    // Element with this custom html property and value contains all messages
+    var messageContainer = document.querySelectorAll('[aria-live="polite"]')[0]
+
+    console.log("Message Container: ")
+    console.log(messageContainer)
+
+    //Add Like button to first message
+    if (messageContainer.children.length != 0) {
+
+      // Create the like button element
+      var likeButton = document.createElement("div");
+      likeButton.id = "likeButtonContainer";
+      likeButton.className = "like-button";
+
+      const myPolicy = trustedTypes.createPolicy('my-policy', {
+        createHTML: (input) => {
+          return input;
+        }
+      });
+
+      const trustedHTML = myPolicy.createHTML(`
+        <button id="likeButton" class="like-icon">&#128077;</button>
+        <span id="likeCounter" class="like-count">0</span>`
+      )
+      likeButton.innerHTML = trustedHTML
+
+      //Add like button element to message
+      var message = messageContainer.children[0]
+      message.appendChild(likeButton);
+
+      //Add event listener for increase like counter
+      var likeCount = 0
+      var likeButton2 = document.getElementById("likeButton");
+      var likeCounter = document.getElementById("likeCounter");
+
+      likeButton2.addEventListener("click", function () {
+        likeCount = 1 - likeCount;
+        likeCounter.textContent = likeCount;
+      });
+    }
+  }
+  
   return (
     <>
       <div>
@@ -28,7 +70,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <input type="color" value={colour} onChange={(e) => setColour(e.target.value)} />
         <button onClick={() => onClick()}>
           Click me
         </button>
